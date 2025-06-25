020bc8cbe1d78f7adda51a22c4705e644e6dd848
AntonYakubovich <86956837+AntonYakubovich@users.noreply.github.com>
2025-06-25 14:16:03 +0300

    Create reset_rtc8564.sh
    Script for RTC clock RTC8564 (pcf8563). Set walid date and time in new IC. 
    Without this OS not work with non valid data from clock

---
+++ reset_rtc8564.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Сбрасываем часы на i2c шине"
+echo "выгружаем драйвер "
+sudo modprobe -r rtc_pcf8563
+echo " Дамп регистров"
+sudo i2cdump -y 0 0x51
+echo "Сбрасываем секунды"
+sudo i2cset 0 0x51 0x02 0x00 -y
+echo "Сбрасываем минуты"
+sudo i2cset 0 0x51 0x03 0x00 -y
+echo "Сбрасываем часы"
+sudo i2cset 0 0x51 0x04 0x00 -y
+echo "Сбрасываем день"
+sudo i2cset 0 0x51 0x05 0x24 -y
+echo "Сбрасываем день недели"
+sudo i2cset 0 0x51 0x06 0x02 -y
+echo "Сбрасываем месяц и ставим тысячелетие"
+sudo i2cset 0 0x51 0x07 0x86 -y
+echo "Сбрасываем год"
+sudo i2cset 0 0x51 0x08 0x25 -y
+echo "Сбрасываем минуты будильника"
+sudo i2cset 0 0x51 0x09 0x00 -y
+echo "Сбрасываем часы будильника"
+sudo i2cset 0 0x51 0x0a 0x00 -y
+echo "Сбрасываем день недели будильника"
+sudo i2cset 0 0x51 0x0b 0x02 -y
+echo "Сбрасываем день недели будильника"
+sudo i2cset 0 0x51 0x0c 0x02 -y
+echo "Сбрасываем clkout register 1 Hz"
+sudo i2cset 0 0x51 0x0d 0x03 -y
+echo "Сбрасываем register 1 0x0e"
+sudo i2cset 0 0x51 0x0e 0x03 -y
+echo "Сбрасываем reg 0x0f"
+sudo i2cset 0 0x51 0x0f 0x00 -y
+echo "Делаем общий ресет"
+sudo i2cset 0 0x51 0x00 0x00 -y
+sudo i2cdump -y 0 0x51
+echo "Загружаем драйвер обратно"
+sudo modprobe rtc-pcf8563
+echo "Показываем время"
+timedatectl
